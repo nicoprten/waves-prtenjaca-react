725e22b22fcf9b8a5848e0ac59ffbc8916a921a7
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2021-09-16 01:53:17 -0300

    Desafío 06 v2 mejorado

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,11 +1,25 @@
 import {useState, useEffect} from 'react';
+import ItemCount from '../ItemCount/ItemCount.js'
+
 function Item(producto){
     const [items, setItems] = useState([]);
-    const [cargando, setCargando] = useState(true);
+    const [cargando, setCargando] = useState(false);
     console.log(producto);
     useEffect(() =>{
+        setCargando(true);
         agarrarProductos().then((result) =>{
-            setItems(result);
+            setItems(
+                <div className='item' key={result.id}>
+                    <h2 className='item__title' >{result.nombre}</h2>
+                    <p className='item__precio'>${result.precio}</p>
+                    <img className='item__img' src={result.srcimg} alt={result.desc}/>
+                    <ItemCount />
+                </div>
+            );
+            
+        })
+        .finally(() =>{
+            setCargando(false);
         });
     }, [])
     const agarrarProductos = () => {
@@ -22,12 +36,14 @@ function Item(producto){
     agarrarProductosAsync();
     return(
         <>
+            {/* <h3>{cargando ? 'Cargando item' : ''}</h3>
             <div className='item' key={items.id}>
                 <h2 className='item__title' >{items.nombre}</h2>
-                <p className='item__precio'>${items.precio}</p>
+                <p className='item__precio'>{items.precio}</p>
                 <p>{items.desc}</p>
-            </div>
-                
+            </div> */}
+            
+                {items}
         </>
     );
 }
--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -1,22 +1,25 @@
-import Item from '../item/Item.js';
+import Item from '../item/Item.js'
 function ItemList(){
     let productos = [
         {
             id: 1,
             nombre: 'Remera Enlighted',
             precio: 2400,
+            srcimg: '//d3ugyf2ht6aenh.cloudfront.net/stores/121/284/products/remera_fallbody11-8045da76c1026d10ff16308897720520-1024-1024.png',
             desc: 'Remera muy fachera limitada.'
         }, 
         {
             id: 2,
-            nombre: 'Pantalon Adidas',
+            nombre: 'Pantalon',
             precio: 1300,
+            srcimg: '//d3ugyf2ht6aenh.cloudfront.net/stores/121/284/products/pantalon_ai_bordado111-840ae6036c78d37aa716263754206574-1024-1024.png',
             desc: 'Muy comodos para salir a correr.'
         }, 
         {
             id: 3,
             nombre: 'Gorro de lana RipCurl',
             precio: 900,
+            srcimg: '//d3ugyf2ht6aenh.cloudfront.net/stores/121/284/products/beanie-logo21-2a1cf6b7a8e808eb9416276009088444-1024-1024.png',
             desc: 'Preparate para el frio con el mejor gorro.'
         }
     ];
@@ -27,12 +30,15 @@ function ItemList(){
             <div className='listado'>
                 {productos?.map((producto) => {
                         return(
-                            <Item 
-                            key={producto.id}
-                            nombre={producto.nombre}
-                            precio={producto.precio}
-                            desc={producto.desc}
-                            />
+                            <>
+                                <Item 
+                                key={producto.id}
+                                nombre={producto.nombre}
+                                precio={producto.precio}
+                                srcimg={producto.srcimg}
+                                desc={producto.desc}
+                                />
+                            </>
                         )
                     })}
             </div>
