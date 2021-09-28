e65fbd7a0494ea67fe6163028b2b4090d25b754d
Nicolas Prtenjaca <nicoprtenjaca@gmail.com>
2021-09-27 23:27:09 -0300

    Keep class 10 context 1:05h

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ import Products from './pages/Products.jsx';
 import Home from './pages/Home.jsx';
 import NotFound from './pages/NotFound.jsx';
 import {Switch, BrowserRouter, Route} from 'react-router-dom';
+import {CartContext} from './context/CartContext.js';
+import {useState} from 'react';
 
 function App() {
+  const [themeColor, setThemeColor] = useState('red');
+  if(themeColor === 'red'){
+    setThemeColor('blueee');
+  }
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route exact path='/products' component={Products}/>
-            <Route exact path='/products/:id' component={ProductDetail}/>
-            <Route exact path='/category' component={Products}/>
-            <Route exact path='/category/:categoryId' component={Products}/>
-            <Route path='*' component={NotFound}/>
-      </Switch>
-      <Footer />
-    </BrowserRouter>
+    <CartContext.Provider value={themeColor}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/products' component={Products}/>
+              <Route exact path='/products/:id' component={ProductDetail}/>
+              <Route exact path='/category' component={Products}/>
+              <Route exact path='/category/:categoryId' component={Products}/>
+              <Route path='*' component={NotFound}/>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
---
+++ src/context/CartContext.js
@@ -0,0 +1,4 @@
+import * as React from 'react';
+
+export const CartContext = React.createContext('blue');
+
